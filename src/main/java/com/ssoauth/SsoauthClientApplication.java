3fff2391523e5b064ec1d9216e7d4bc9f3aecb79
dh.kim <dh.kim@cmsedu.co.kr>
2020-03-23 20:24:32 +0900

    spring init

---
+++ src/main/java/com/ssoauth/SsoauthClientApplication.java
@@ -0,0 +1,13 @@
+package com.ssoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoauthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoauthClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssoauth/SsoauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoauthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
